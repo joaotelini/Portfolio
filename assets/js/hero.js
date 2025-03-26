c26e663fe5ba94727ba100e76b78d8cb114b7f13
João Pedro Telini <joaotelini03@gmail.com>
2025-03-26 19:18:39 -0300

    first commit

---
+++ assets/js/hero.js
@@ -0,0 +1,8 @@
+const btnHero = document.getElementById("btnHero");
+
+btnHero.addEventListener("click", () => {
+  window.open(
+    "https://drive.google.com/file/d/1RTUGLW1IUIv8xZckvzMBtdGlkyU5v_-h/view?usp=sharing",
+    "_blank"
+  );
+});
