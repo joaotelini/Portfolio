26a56c49d105198a5b74bb7ff9bb5998c9770687
João Pedro Telini <joaotelini03@gmail.com>
2025-03-31 17:02:40 -0300

    ft button to download cv

--- assets/js/downloadCV.js
+++ assets/js/downloadCV.js
@@ -1,8 +1,6 @@
-const btnHero = document.getElementById("btnHero");
-
-btnHero.addEventListener("click", () => {
+window.downloadCV = function () {
   window.open(
     "https://drive.usercontent.google.com/download?id=1RTUGLW1IUIv8xZckvzMBtdGlkyU5v_-h&export=download&authuser=0",
     "_blank"
   );
-});
+};
