85f2769e22e814d62b1799278d4bb8fff65c0177
João Pedro Telini <joaotelini03@gmail.com>
2025-04-04 22:55:43 -0300

    ft add status message display for contact form submission

--- assets/js/emailJs.js
+++ assets/js/emailJs.js
@@ -1,5 +1,6 @@
 const serviceID = "service_1kvbrfi";
 const templateID = "template_anrlz5m";
+const pstatus = document.getElementById("p-status");
 
 (function () {
   emailjs.init("ar81o83bD0Ij51w8t");
@@ -12,13 +13,16 @@ document
 
     emailjs.sendForm(serviceID, templateID, this).then(
       function () {
-        alert("Your message has been sent successfully!");
       },
       function (error) {
-        alert("Failed to send your message. Please try again later.");
-        console.error("Error sending email:", error);
+        pstatus.innerHTML = "Error sending message. Please try again later.";
+        console.log("FAILED...", error);
       }
     );
 
     this.reset();
+    pstatus.innerHTML = "Sending...";
+    setTimeout(() => {
+      pstatus.innerHTML = "Message sent!";
+    }, 3000);
   });
